12057feead17af821d5bcbbccb8cbfacf48a8e10
markedin <119455170+markedin@users.noreply.github.com>
2023-05-14 14:25:12 -0700

    Create inheritance submission assignment
    this submission includes a parent class and 2 inheriting child classes

---
+++ InheritanceSubmissionAssignment/inheritancesubmissionassignment.py
@@ -0,0 +1,12 @@
+class Animal:
+    name = ''
+    weight = ''
+    height = ''
+
+class Mammal(Animal):
+    litter_size = ''
+    hibernate = False
+
+class Fish(Animal):
+    egg_count = ' '
+    scale_type = ' ' 
